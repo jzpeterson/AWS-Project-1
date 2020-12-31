418274b14028ffdb494ade65994c528349e3a0f3
jzpeterson <jzpeterson@mastercontrol.com>
2020-12-30 23:13:01 -0700

    Update cognito information

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_a1gd8Rbr4', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_KIK1fRoxh', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '797nf82f5m4bp3agv1kdo2gsel', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
